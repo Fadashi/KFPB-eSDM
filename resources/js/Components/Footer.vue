540926ce095cb97dac42dbeba29b32357b85034b
Muhammad Faishal Daffa Sya'bani <faishaldaffa16@gmail.com>
2025-04-08 07:47:49 +0700

    Update Page Admin Employees (Change to Tailwind Css)

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-gray-800 text-white py-4">
     <div class="container mx-auto text-center">
       <p class="text-sm">
-        &copy; {{ new Date().getFullYear() }} Perusahaan Anda. Semua hak dilindungi.
+        &copy; {{ new Date().getFullYear() }} Sistem eSDM Kimia Farma Plant Banjaran
       </p>
       <div class="flex justify-center space-x-4 mt-2">
         <a href="#" class="hover:underline">Tentang Kami</a>
